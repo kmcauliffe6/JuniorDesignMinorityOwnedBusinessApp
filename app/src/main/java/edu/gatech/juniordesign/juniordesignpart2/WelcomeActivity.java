7652f5fc77cd874d54a7c30b1678bf435ce70333
kmcauliffe6 <mcauliffek@gatech.edu>
2019-01-14 13:52:02 -0500

    finished Welcome page UI, functioning LogIn and Guest buttons

--- app/src/main/java/edu/gatech/juniordesign/juniordesignpart2/WelcomeActivity.java
+++ app/src/main/java/edu/gatech/juniordesign/juniordesignpart2/WelcomeActivity.java
@@ -1,7 +1,9 @@
 package edu.gatech.juniordesign.juniordesignpart2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+    }
+
+    /**
+     * goes to LoginActivity
+     * @param view the current view
+     *
+     */
+    public void goToLoginActivity (View view) {
+        Intent intent = new Intent (this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * goes to MainActivity when user selects continue as guest
+     * @param view the current view
+     *
+     */
+    public void goToMainActivity (View view) {
+        Intent intent = new Intent (this,MainActivity.class);
+        startActivity(intent);
     }
 }
