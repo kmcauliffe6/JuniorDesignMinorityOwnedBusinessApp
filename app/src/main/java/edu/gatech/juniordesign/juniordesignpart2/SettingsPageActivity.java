8841c0e9a6dae9d6eb30a9cdae2812f1bd3fbab8
niruku7 <niruku7@gatech.edu>
2019-01-18 23:24:52 -0500

    updates for the terms page

--- app/src/main/java/edu/gatech/juniordesign/juniordesignpart2/SettingsPageActivity.java
+++ app/src/main/java/edu/gatech/juniordesign/juniordesignpart2/SettingsPageActivity.java
@@ -3,12 +3,20 @@ package edu.gatech.juniordesign.juniordesignpart2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.view.View;
+
 public class SettingsPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settingspage);
+    }
+
+    public void goToTermsActivity (View view){
+        Intent intent = new Intent (this, TermsActivity.class);
+        startActivity(intent);
     }
 
 }
