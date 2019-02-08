dba07d53a91b4206117e74149469e801c885581e
kmcauliffe6 <mcauliffek@gatech.edu>
2019-02-08 16:35:02 -0500

    finished code for tabs on business detail page -Paige

--- app/src/main/java/edu/gatech/juniordesign/juniordesignpart2/BusinessDetailPageActivity.java
+++ app/src/main/java/edu/gatech/juniordesign/juniordesignpart2/BusinessDetailPageActivity.java
@@ -1,42 +1,11 @@
 package edu.gatech.juniordesign.juniordesignpart2;
 
-import android.support.constraint.Placeholder;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
 import android.widget.TabHost;
-import android.widget.TextView;
+
 
 public class BusinessDetailPageActivity extends AppCompatActivity {
-
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
-     */
-    private BusinessDetailPageActivity.SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    private ViewPager mViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,14 +27,6 @@ public class BusinessDetailPageActivity extends AppCompatActivity {
         ts.setContent(R.id.tab3);
         ts.setIndicator("About The Owner");
         tabhost.addTab(ts);
-
-        /** Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new BusinessDetailPageActivity.SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter); **/
     }
     /** info needed:
      * business name
@@ -76,41 +37,6 @@ public class BusinessDetailPageActivity extends AppCompatActivity {
      * photos
      * about the owners
      */
-
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
-
-        @Override
-        public Fragment getItem(int position) {
-
-            switch (position) {
-                case 0:
-                    ReviewTab rt = new ReviewTab();
-                    return rt;
-                case 1:
-                    BusinessPhotosTab bpt = new BusinessPhotosTab();
-                    return bpt;
-                case 2:
-                    AboutTheOwnerTab ao = new AboutTheOwnerTab();
-                    return ao;
-                default:
-                    return null;
-            }
-        }
-
-        @Override
-        public int getCount() {
-            //bc we have 3 tabs
-            return 3;
-        }
-    }
 
 }
 
