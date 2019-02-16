5abdcf2836d9cfa1414b96262ea907c709627718
amayers31 <Vaderisking7@gmail.com>
2019-02-15 19:24:06 -0500

    Finished homepage and addded category handling in Intent

--- app/src/main/java/edu/gatech/juniordesign/juniordesignpart2/MainActivity.java
+++ app/src/main/java/edu/gatech/juniordesign/juniordesignpart2/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void goToBusinessListActivity (View view){
+        Intent intent = new Intent (this, BusinessListActivity.class);
+        Button clicked = (Button) view;
+        String category = clicked.getText().toString();
+        intent.putExtra("category", category);
+        startActivity(intent);
+    }
 
     public void goToSettingsPageActivity (View view){
         Intent intent = new Intent (this, SettingsPageActivity.class);
