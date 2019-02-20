b2fe58132d59e90225c75837680cc896fc9ccc72
kmcauliffe6 <mcauliffek@gatech.edu>
2019-02-20 14:00:27 -0500

    replaced Settings button on home screen with a icon in the ActionBar

--- app/src/main/java/edu/gatech/juniordesign/juniordesignpart2/MainActivity.java
+++ app/src/main/java/edu/gatech/juniordesign/juniordesignpart2/MainActivity.java
@@ -1,9 +1,13 @@
 package edu.gatech.juniordesign.juniordesignpart2;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ActionMenuView;
 import android.widget.Button;
 
 
@@ -13,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void goToBusinessListActivity (View view){
@@ -31,6 +36,24 @@ public class MainActivity extends AppCompatActivity {
     public void goToProfilePageActivity (View view) {
         Intent intent = new Intent (this, ProfilePageActivity.class);
         startActivity(intent);
+    }
+
+    // create an action bar button
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.my_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // handle button activities
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.mybutton) {
+            goToSettingsPageActivity(getWindow().getDecorView().getRootView());
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
